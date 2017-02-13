e8d52db8fc0c8a754ff3cdd8b0ea83334c2a124e
valentiespejo <valentiesro@gmail.com>
2017-02-13 20:38:48 +0100

    Control Structures

--- Math_is_fun_7/Math_is_fun_7.ino
+++ Math_is_fun_7/Math_is_fun_7.ino
@@ -11,18 +11,17 @@
 //define the variable
 int test = 32767;
 //******************************** SETUP ******************************************
-void setup()             
+void setup()
 {
   Serial.begin(9600);      // run one when the arduino starts
   Serial.print("Test value is:"); //First line on terminal
   Serial.println(test); //Read the variable in the same line
 
   test = test + 1; // Plus one to variable 32767
- 
+
   Serial.print("Now it is "); //Second line on terminal
   Serial.println(test); // the result number
-//* 32767 is the last number on a 32 bytes variable and the number 
- converts to negative until 32767*//
+  //* 32767 is the last number on a 32 bytes variable and the number
 }
 
 //********************************* LOOP ******************************************
