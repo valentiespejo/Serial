b5dd970e1eb92ce30060593bd3883e46ee2100d0
valentiespejo <valentiesro@gmail.com>
2017-02-27 19:44:37 +0100

    Control Structure 4

---
+++ Control_Structure_4/Control_Structure_4.ino
@@ -0,0 +1,51 @@
+/**********************************************************************************
+**                                                                               **
+**                              Control Structures 4                             **
+**                                                                               **
+** VALENTÍ ESPEJO RODRIGUEZ                                    27/02/2017        **
+**********************************************************************************/
+
+//******************************* INCLUDES ****************************************
+
+//******************************* VARIABLES ***************************************
+float qualEnergy = 10; // define the variable in a decimal form
+//******************************** SETUP ******************************************
+void setup()              // run once, when the arduino starts
+{
+  Serial.begin(9600); 
+  
+  if (qualEnergy >= 43.2) //when the variables i bigger or 43.2 G
+  {
+    Serial.print("G");
+  }
+  else if (qualEnergy < 43.2 & qualEnergy > 38.2)//between 6.5 - 3.5 print F
+  {
+    Serial.print("F");
+  }
+  else if (qualEnergy <= 38.2 & qualEnergy > 17.7)//between 6.5 - 3.5 print E
+  {
+    Serial.print("E");
+  }
+  else if (qualEnergy <= 17.7 & qualEnergy > 11.1)//between 6.5 - 3.5 print D
+  {
+    Serial.print("D");
+  }
+  else if (qualEnergy <= 11.1 & qualEnergy > 6.5)//between 6.5 - 3.5 print C
+  {
+    Serial.print("C");
+  }
+  else if (qualEnergy <= 6.5 & qualEnergy > 3.5)//between 6.5 - 3.5 print B
+  { 
+    Serial.print("B");
+  }
+  else
+  {
+    Serial.print("A"); //if the variable is less than 3,5 print
+  }
+}
+
+//********************************* LOOP ******************************************
+void loop()  // no program in loop part
+{
+}
+//******************************* FUNCIONS ****************************************
