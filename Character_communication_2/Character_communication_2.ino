73fd76ccd581bb0ee3843e141df766aa385d89cb
valentiespejo <valentiesro@gmail.com>
2017-03-06 16:09:13 +0100

    Charcter communication

--- Character_communication_2/Character_communication_2.ino
+++ Character_communication_2/Character_communication_2.ino
@@ -20,34 +20,34 @@ void setup() {
 }
 //********************************* LOOP ******************************************
 void loop()
-  {
+{ 
   while (Serial.available() > 0)
   {
-    thisByte = thatByte;
-    thatByte = Serial.read();
+    thisByte = thatByte; 
+    thatByte = Serial.read(); 
     if (thatByte == '\n')
     {
       if (thisByte > 96 &  thisByte < 123)
       {
         Serial.println("");
-        Serial.print("La lletra ");
-        Serial.write(thisByte);
+        Serial.print("La lletra ");  
+        Serial.write(thisByte); 
         Serial.println(" esta en minuscula");
       }
       else if (thisByte > 64 &  thisByte < 91)
       {
         Serial.println("");
-        Serial.print("La lletra ");
-        Serial.write(thisByte);
+        Serial.print("La lletra ");  
+        Serial.write(thisByte); 
         Serial.println(" esta en majuscula");
       }
       else
       {
         Serial.println("No has entrat una lletra!");
-      }
-    Serial.println("");
-    Serial.println("Introdueixi lletra de l'alfabet en majuscula o minuscula...");
+      } 
+      Serial.println("");
+      Serial.println("Introdueixi lletra de l'alfabet en majuscula o minuscula...");      
     }
-  }
-  }
+  }  
+} 
 //******************************* FUNCIONS ****************************************
